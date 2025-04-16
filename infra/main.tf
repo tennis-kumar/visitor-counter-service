e76929c97edbbc538cd5b6be8f3d2781438e0158
tennis <kumarteneesh@gmail.com>
2025-04-16 16:41:55 +0530

    Add security group configuration and update EC2 instance provisioning in Terraform

--- infra/main.tf
+++ infra/main.tf
@@ -7,10 +7,40 @@ resource "aws_key_pair" "ec2_key" {
   public_key = file("${path.module}/ec2_key.pub")
 }
 
+resource "aws_security_group" "visitor_sg" {
+  name = "visitor-sg"
+  description = "Allow HTTP and SSH traffic"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  
+}
+
 resource "aws_instance" "visitor_ec2" {
   ami = var.ubuntu_ami
   instance_type = "t2.micro"
   key_name = aws_key_pair.ec2_key.key_name
+  vpc_security_group_ids = [ aws_security_group.visitor_sg.id ]
+  associate_public_ip_address = true
 
   tags = {
     Name = "VisitorCounterApp"
@@ -21,14 +51,16 @@ resource "aws_instance" "visitor_ec2" {
       type = "ssh"
       user = "ubuntu"
       private_key = file("${path.module}/ec2_key")
-        host = self.public_ip
+      host = self.public_ip
+      timeout = "5m"
     }
 
     inline = [ 
         "sudo apt update",
         "sudo apt install -y docker.io",
         "sudo systemctl enable docker",
-        "sudo docker run -d -p 80:3000 ghcr.io/tennis-kumar/visitor-counter-service"
+        "sudo systemctl start docker",
+        "sudo docker run -d -p 80:3000 ghcr.io/tennis-kumar/visitor-counter-service:latest"
      ]
   }
 }
